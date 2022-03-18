613cd4f5df4c5c334eaefe87ad772d2e39bb09da
RailonA <railonacosta@gmail.com>
2022-03-18 09:28:13 -0400

    Add instructions for Mac/Darwin- need to test on a mac computer

--- deployHttps.js
+++ deployHttps.js
@@ -7,7 +7,7 @@ const { exec } = require('child_process');
 // console.log(os.release()); // "10.0.14393"
 // console.log(os.platform()); // "win32"
 
-if(os.type()==="Linux"){
+if(os.type() === "Linux"){
     // sudo apt-get update;
         exec("sudo apt-get update", (error, stdout, stderr) => {
         if (error) {
@@ -49,7 +49,6 @@ if(os.type()==="Linux"){
 
     // source ~/.bash_profile;
     // touch ~/.bash_profile;
-
     exec("touch ~/.bash_profile", (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
@@ -76,6 +75,72 @@ if(os.type()==="Linux"){
         });      
           
     
+} else if (os.type() === "Darwin") {
+    // sudo apt-get update;
+    exec("brew update", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+      });
+      
+      // sudo apt-get install openssl;
+      exec("brew install openssl", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+      });
+      
+      // echo 'export PATH="/usr/local/opt/openssl/bin:$PATH"' >> ~/.bash_profile
+      exec(`echo 'export PATH="/usr/local/opt/openssl/bin:$PATH"' >> ~/.bash_profile`, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+      });
+      
+      // source ~/.bash_profile;
+      // touch ~/.bash_profile;
+      exec("source ~/.bash_profile", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+      });
+      
+    // yes "" | openssl req -nodes -new -x509 -keyout server.key -out server.cert;
+      exec("yes '' | openssl req -nodes -new -x509 -keyout server.key -out server.cert", {maxBuffer: 1024 * 500}, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+      });
 }
 else {
 console.log(os.type())
